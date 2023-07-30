f0785ca60e99ec4bec205f1431fc01cf2e7860ab
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-07-30 21:50:24 +0700

    implement darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg={"darkgrey"}>
+        <GridItem area="aside" bg={"gold"}>
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg={"orange"}>
+      <GridItem area="main" bg={"navy"}>
         Main
       </GridItem>
     </Grid>
