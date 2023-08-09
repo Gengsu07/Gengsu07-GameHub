bb890d21f06545b12bbe0e17f3fbb489f3f9e895
Gengsu07 <sugengdatascience@gmail.com>
2023-08-09 13:58:07 +0700

    handle no Image

--- src/services/ImageUrl.ts
+++ src/services/ImageUrl.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImg = (url: string) => {
-  if (!url) return; //kalau ada game yg tidak ada gambarnya biar tidak error
+  if (!url) return noImage; //kalau ada game yg tidak ada gambarnya biar tidak error
   const delim = "media/";
   const index = url.indexOf(delim) + delim.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
